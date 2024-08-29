cac53664e0c8f1157253f5ab1af35c367e976838
Aluno <sofiacuetobarros@gmail.com>
2024-08-29 19:18:36 -0300

    Incluindo arquivo operadores_relacionais

---
+++ operadores_relacionais.py
@@ -0,0 +1,26 @@
+a = 5
+b = 3
+
+#igualdade
+resultado = a == b
+print(resultado)
+
+#Diferença
+resultado = a != b
+print(resultado)
+
+#Maior que (>)
+resultado = a > b
+print(resultado)
+
+#Menor que (<)
+resultado = a < b
+print(resultado)
+
+#Maior ou igual (>=)
+resultado = a >= b
+print(resultado)
+
+#Menor ou igual (<=)
+resultado = a <= b
+print(resultado)
