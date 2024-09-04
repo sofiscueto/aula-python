bbadf1fe0b81199dc2924876f4c77bed5b2473d3
sofiscueto <sofiacuetobarros@gmail.com>
2024-09-04 20:33:08 -0300

    Atualizar arquivo refatoracao-ativ1

--- refatoracao-ativ1.py
+++ refatoracao-ativ1.py
@@ -5,11 +5,14 @@ valores = [100, 200, 300]
 totalInicial = sum(valores)
 print(f"Total do preço: {totalInicial}") 
 
-desconto = 0
 #Aqui ele aplicara um desconto de 10%  caso a compra seja maior que 500, fazendo o calculo do desconto
+desconto = 0
 if totalInicial > 500: 
     desconto = totalInicial * 0.1 
-totalDesconto = totalInicial - desconto 
+    totalDesconto = totalInicial - desconto 
+    print(f"Desconto aplicado: {desconto}")
+    print(f"Total com desconto: {totalDesconto}")
 
-print(f"Desconto aplicado: {desconto}")
-print(f"Total com desconto: {totalDesconto}")
+else:
+    diferenca = 500 - totalInicial
+    print(f"Faltou {diferenca} para o desconto ser aplicadp! Deseja incluir mais produtos?")
