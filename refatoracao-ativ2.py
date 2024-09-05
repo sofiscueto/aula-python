819e50c0681e817d14fe6a8e37591d150ca2fa91
sofiscueto <sofiacuetobarros@gmail.com>
2024-09-04 21:06:53 -0300

    Incluir arquivo refatoracao-ativ2

---
+++ refatoracao-ativ2.py
@@ -0,0 +1,9 @@
+idade = 25
+
+categoria = "Indefinida"
+if idade > 13:
+    categoria = "criança"
+elif idade >+ 13 and idade < 18:
+    categoria = "Adolescente"
+
+print("A pessoa é classificada como: ",categoria)
