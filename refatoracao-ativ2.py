786c29ea9fa76e5df51bb079f42d130d1ba4fc43
sofiscueto <sofiacuetobarros@gmail.com>
2024-09-04 21:26:29 -0300

    Atualizar arquivo refatoracao-ativ2

--- refatoracao-ativ2.py
+++ refatoracao-ativ2.py
@@ -1,9 +1,16 @@
-idade = 25
+#Usuario vai informar a idade
+idade = int(input("Digite sua idade: "))
 
+#Aqui são definidas as classificações da faixa etária
 categoria = "Indefinida"
-if idade > 13:
-    categoria = "criança"
-elif idade >+ 13 and idade < 18:
+if idade < 13:
+    categoria = "Criança"
+elif idade >= 13 and idade < 18:
     categoria = "Adolescente"
+elif idade >= 18 and idade < 60:
+    categoria = "Adulto"
+elif idade >= 60:
+    categoria = "Idoso"
 
-print("A pessoa é classificada como: ",categoria)
+#Apresentará o resultado da consulta 
+print(f"O usuário é classificado como {categoria}")
