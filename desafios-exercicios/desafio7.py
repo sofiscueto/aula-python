d953f63110ec3181468f19d08dabef9bb3b6f2ab
sofiscueto <sofiacuetobarros@gmail.com>
2024-09-12 19:29:20 -0300

    Atualização do arquivo do desafio7

--- desafios-exercicios/desafio7.py
+++ desafios-exercicios/desafio7.py
@@ -1,6 +1,6 @@
 #Manipulação de Dicionários/Objetos: Criar um dicionário onde as chaves são nomes de alunos e os valores são suas notas finais. O programa deve pedir ao usuário o nome de um aluno e exibir a nota desse aluno.
 # Para esse desafio utilize
-# ● Utilize if da seguinte maneira
+# ● Utilize if da seguinte maneira:
 #   if aluno in notas:
 #    Esse if tem o significado de = “Se aluno que foi digitado existe na lista = True”
 
