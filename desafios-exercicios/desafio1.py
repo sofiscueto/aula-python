b6b761a6ec3fef7dd57d308fb89e67c9d92b067a
sofiscueto <sofiacuetobarros@gmail.com>
2024-09-12 19:22:31 -0300

    Atualização do arquivo da aula05

--- desafios-exercicios/desafio1.py
+++ desafios-exercicios/desafio1.py
@@ -1,4 +1,4 @@
-#Concaternar e Repetir: Criar um programa que solicite o nome e sobrenome e, em seguida, exiba uma mensagem de bosas-vindas cpmpleta no formato; "Bem-vindo, [Nome][Sobrenome]!""
+#Concaternar e Repetir: Criar um programa que solicite o nome e sobrenome e, em seguida, exiba uma mensagem de bosas-vindas cpmpleta no formato; "Bem-vindo, [Nome][Sobrenome]!"
 
 nome = input("Digite seu nome: ")
 sobrenome = input("Digite seu Sobrenome: ")
