f760552af95d1addd0d679d8478f1512626e4e74
sofiscueto <sofiacuetobarros@gmail.com>
2024-09-12 19:26:52 -0300

    Atualização do arquivo do desafio2

--- desafios-exercicios/desafio2.py
+++ desafios-exercicios/desafio2.py
@@ -1,4 +1,4 @@
-#Concatenar e Repetir: Criar um programa que receba uma palavra do usuário e um número inteiro. O programa deve imprimir a palavra repetida o número de vezes especificado.
+#Concatenar e Repetir: Criar um programa que receba uma palavra do usuário e um número inteiro. O programa deve imprimir a palavra repetida o número de vezes especificado..
 
 palavra = input("Digite uma palavra: ")
 numero = int(input("Digite um número: "))
